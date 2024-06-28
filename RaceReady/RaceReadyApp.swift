01247b32aeba274e5b8dfeaaf5892b87139069f7
AdnannM <adnannmuratovic@icloud.com>
2024-06-28 21:11:17 +0200

    Initial Commit

---
+++ RaceReady/RaceReadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceReadyApp.swift
+//  RaceReady
+//
+//  Created by Adnann Muratovic on 28.06.24.
+//
+
+import SwiftUI
+
+@main
+struct RaceReadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
