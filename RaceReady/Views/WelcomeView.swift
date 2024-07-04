efd962a931914f44528c16ef02d4218c4084a362
AdnannM <adnannmuratovic@icloud.com>
2024-07-04 16:29:11 +0200

    feat: update ui on race view

--- RaceReady/Views/WelcomeView.swift
+++ RaceReady/Views/WelcomeView.swift
@@ -12,31 +12,35 @@ struct WelcomeView: View {
     @State private var isPresented: Bool = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            Button(action: {
-                withAnimation(.easeInOut(duration: 0.5)) {
-                    isPresented = true
+        ZStack {
+            LinearGradient(colors: [.blue, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+              .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        isPresented = true
+                    }
+                }) {
+                    Text("Continue")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: 250)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(16)
                 }
-            }) {
-                Text("Continue")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: 250)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(16)
             }
-        }
-        .background(Color.white)
-        .fullScreenCover(isPresented: $isPresented, content: {
-            MainTabbedView()
-                .transition(.move(edge: .trailing))
-        })
+            .fullScreenCover(isPresented: $isPresented, content: {
+                MainTabbedView()
+                    .transition(.move(edge: .trailing))
+            })
+            
         .animation(.easeInOut(duration: 0.5), value: isPresented)
+        }
     }
 }
 
 #Preview {
-    WelcomeView()
+    WelcomeView().environmentObject(SeasonModel(webservice: WebService()))
 }
